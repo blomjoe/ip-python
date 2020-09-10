1e46130e82e5a0de5e56550aa77bf16bdcc0263a
blomjoe <64746667+blomjoe@users.noreply.github.com>
2020-09-10 15:10:41 +0300

    Update IP-PYTHON-v1.0.py

--- IP-PYTHON-v1.0.py
+++ IP-PYTHON-v1.0.py
@@ -19,22 +19,29 @@ print(f"IP Address: {ip_address}")
 ## from module requests we import get
 from requests import get
 
+## with help of ipify, we get public ip
 ip = get('https://api.ipify.org').text
 print('Public IP address: {}'.format(ip))
  
 ## import modules re & uuid  
 import re, uuid 
   
-# joins elements of getnode() after each 2 digits. 
-# using regex expression 
+## joins elements of getnode() after each 2 digits. 
+## using regex expression
+## MAC address output in better formatting 
 print ("The MAC address: ", end="") 
 print (':'.join(re.findall('..', '%012x' % uuid.getnode())))
 
-print("-----------------------------------------")
+## print line break (obvious)
+print("-----------------------------------")
 
+## print text to exit program
 print("Press enter to exit the program.")
 
+## import keyboard module
 import keyboard
 
+## from keyboard module we use wait function to use enter button to exit program on press
 keyboard.wait("enter")
 
+## END OF PROGRAM
