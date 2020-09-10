5403a02cdc1fae8a98c7b339f3b4f29fd34f6b03
blomjoe <64746667+blomjoe@users.noreply.github.com>
2020-09-10 12:33:34 +0300

    Create IP-PYTHON-v1.0.py

---
+++ IP-PYTHON-v1.0.py
@@ -0,0 +1,31 @@
+## IP-PYTHON v1.0 by blomjoe
+
+## import socket module
+import socket
+
+## import requests module
+import requests
+
+## getting hostname by socket.gethostname() method
+hostname = socket.gethostname()
+
+## getting  IP address using socket.gethostbyname() method
+ip_address = socket.gethostbyname(hostname)
+
+## printing the hostname and ip_address
+print(f"Hostname: {hostname}")
+print(f"IP Address: {ip_address}")
+
+## from module requests we import get
+from requests import get
+
+ip = get('https://api.ipify.org').text
+print('Public IP address: {}'.format(ip))
+ 
+## import modules re & uuid  
+import re, uuid 
+  
+# joins elements of getnode() after each 2 digits. 
+# using regex expression 
+print ("The MAC address in formatted and less complex way is : ", end="") 
+print (':'.join(re.findall('..', '%012x' % uuid.getnode()))) 
